8ddc3a83e2c93bd70344e2986cea23542360724d
Manas Sharma <manas.git@idesignmylife.net>
2026-06-08 10:24:11 +0000

    Added background tasks for grading and scheduling

--- enrollment/enrollment/doctype/assignment/assignment.py
+++ enrollment/enrollment/doctype/assignment/assignment.py
@@ -1,6 +1,8 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
+from frappe.utils.background_jobs import enqueue
+
 
 class Assignment(Document):
 
@@ -20,12 +22,48 @@ class Assignment(Document):
                 "Selected student is not enrolled in the selected course and semester."
             )
 
+    def on_update(self):
+
+        if self.status == "Completed" and not self.graded:
+            enqueue(
+                update_grade,
+                assignment=self.name
+            )
 
 
+def update_grade(assignment):
+
+    doc = frappe.get_doc(
+        "Assignment",
+        assignment
+    )
+
+    grade_map = {
+        "Low": 30,
+        "Medium": 50,
+        "High": 95
+    }
+
+    grade = grade_map.get(doc.priority, 0)
+
+    frappe.db.set_value(
+        "Student",
+        doc.student,
+        "grade",
+        grade
+    )
+
+    frappe.db.set_value(
+        "Assignment",
+        assignment,
+        "graded",
+        1
+    )
+
+    frappe.db.commit()
 
 
 @frappe.whitelist()
-
 def make_todo(source_name, target_doc=None):
 
     def set_hardCoded_values(source, target):
@@ -64,6 +102,8 @@ def get_courses(doctype, txt, searchfield, start, page_len, filters):
     )
 
     return [[d.course] for d in enrollments]
+
+
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
 def get_semesters(doctype, txt, searchfield, start, page_len, filters):
